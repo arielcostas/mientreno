9f5ae6c105eb69b680327e07789b73d836f2f5a4
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-03-08 18:23:35 +0100

    Entity

---
+++ Server/Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models;
+
+public class Usuario
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Login { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+
+    public string Email { get; set; }
+    public string? CodigoVerificacionEmail { get; set; }
+    public bool EmailVerificado { get; set; } = false;
+
+    public string Contraseña { get; set; }
+    public bool RequiereCambioContraseña { get; set; } = false;
+
+    public string SemillaMfa { get; set; }
+    public bool MfaHabilitado { get; set; } = false;
+    public bool MfaVerificado { get; set; } = false;
+
+    public Usuario(Guid id, string login, string nombre, string apellidos, string email,
+        string? codigoVerificacionEmail, bool emailVerificado, string contraseña, bool requiereCambioContraseña,
+        string semillaMfa, bool mfaHabilitado, bool mfaVerificado)
+    {
+        Id = id;
+        Login = login;
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Email = email;
+        CodigoVerificacionEmail = codigoVerificacionEmail;
+        EmailVerificado = emailVerificado;
+        Contraseña = contraseña;
+        RequiereCambioContraseña = requiereCambioContraseña;
+        SemillaMfa = semillaMfa;
+        MfaHabilitado = mfaHabilitado;
+        MfaVerificado = mfaVerificado;
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,17 +1,19 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Server;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseHsts();
 
 app.UseHttpsRedirection();
 
