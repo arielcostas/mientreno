10780246b52caa82a82247a7e6fd5904062b2ebb
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-03-09 11:04:13 +0100

    Sigue

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Server;
@@ -6,10 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.Audience = "Server";
+    })
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "sid";
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    })
+    .AddTwoFactorRememberMeCookie();
 
 var app = builder.Build();
 
@@ -19,7 +40,11 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseRouting();
+app.MapControllers();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
---
+++ Server/RestParams/AutParams.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.RestParams;
+
+public class LoginInput
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? TwoFactorCode { get; set; }
+}
