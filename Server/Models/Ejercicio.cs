67aabdc24aa2c9651bc3418977bf8c471b157e51
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-04-08 21:09:32 +0200

    ª

--- Server/Models/Ejercicio.cs
+++ Server/Models/Ejercicio.cs
@@ -12,7 +12,7 @@ public class Ejercicio
     public int Id { get; set; }
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
-    public string VideoUrl { get; set; }
+    public string? VideoUrl { get; set; }
 
     public Entrenador Owner { get; set; }
     public Categoria? Categoria { get; set; }
