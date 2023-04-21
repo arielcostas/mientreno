9815b07c853cf9b1e86ceb74662a84274e6f04b7
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-04-21 13:46:18 +0200

    Kosas

--- QueueWorker/MailQueueWorker.cs
+++ QueueWorker/MailQueueWorker.cs
@@ -1,21 +1,62 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Reflection;
+using System.Text;
+
 namespace QueueWorker
 {
     public class MailQueueWorker : BackgroundService
     {
         private readonly ILogger<MailQueueWorker> _logger;
+        private readonly IConnection _connection;
 
-        public MailQueueWorker(ILogger<MailQueueWorker> logger)
+        private IModel? _channel;
+
+        public MailQueueWorker(ILogger<MailQueueWorker> logger, IConnection rabbitConnection)
         {
+            _connection = rabbitConnection;
             _logger = logger;
+            
+            _logger.LogInformation("Prepared");
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting...");
+
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare("mientreno", ExchangeType.Direct, true, false);
+            _channel.QueueDeclare("email", true, false, false);
+            _channel.QueueBind("email", "mientreno", "email");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += OnReceived;
+            if (consumer.IsRunning)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                _logger.LogInformation("Consumer running");
             }
+
+            _channel.BasicConsume("email", false, consumer);
+        }
+
+        private void OnReceived(object? sender, BasicDeliverEventArgs e)
+        {
+            var bodyBytes = e.Body.ToArray();
+            var bodyString = Encoding.Default.GetString(bodyBytes);
+            Console.WriteLine(bodyString);
+            _channel?.BasicAck(e.DeliveryTag, false);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping model...");
+            _channel?.Close();
+            _logger.LogInformation("Model stopped...");
+            return base.StopAsync(cancellationToken);
         }
     }
 }
--- QueueWorker/Program.cs
+++ QueueWorker/Program.cs
@@ -1,11 +1,24 @@
 using QueueWorker;
+using RabbitMQ.Client;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+IHostBuilder hb = Host.CreateDefaultBuilder(args);
+
+hb.ConfigureServices((context, services) =>
+{
+    var rabbitConnectionString = context.Configuration.GetConnectionString("RabbitMQ") ?? throw new Exception("RabbitMQ Connection String not set");
+
+    services.AddSingleton((sp) =>
     {
-        services.AddHostedService<MailQueueWorker>();
-        services.AddHostedService<UserDeletionQueueWorker>();
-    })
-    .Build();
+        return new ConnectionFactory()
+        {
+            Uri = new Uri(rabbitConnectionString),
+        }.CreateConnection();
+    });
+
+    services.AddHostedService<MailQueueWorker>();
+    //services.AddHostedService<UserDeletionQueueWorker>();
+});
+
+var host = hb.Build();
 
 host.Run();
