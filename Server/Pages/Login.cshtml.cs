b9f2de2509f06868a506e53b56cbda9117ec09fa
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-06-12 13:45:13 +0200

    Arreglillos de estilo (en proceso)

--- Server/Pages/Login.cshtml.cs
+++ Server/Pages/Login.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Mientreno.Compartido.Recursos;
@@ -18,11 +19,8 @@ public class LoginModel : PageModel
 	}
 
 	[BindProperty] public LoginForm Form { get; set; } = new();
-	
-	public string MensajeError { get; set; } = string.Empty;
 
-	[FromQuery]
-	public string? ReturnUrl { get; set; }
+	[BindProperty(SupportsGet = true)] public string? ReturnUrl { get; set; }
 
 	public IActionResult OnGet()
 	{
@@ -35,7 +33,7 @@ public class LoginModel : PageModel
 
 		return Page();
 	}
-	
+
 	public async Task<IActionResult> OnPost()
 	{
 		var usuario = _userManager.GetUserAsync(User).Result;
@@ -44,38 +42,48 @@ public class LoginModel : PageModel
 			var area = usuario is Alumno ? "Alumnos" : "Dashboard";
 			return Redirect($"/{area}");
 		}
-		
+
 		if (!ModelState.IsValid) return Page();
-	
+
 		var user = await _userManager.FindByEmailAsync(Form.Email);
-		
+
 		if (user is null)
 		{
-			MensajeError = AppStrings.Error_IncorrectCredentials;
+			ModelState.AddModelError(nameof(Form.Contraseña), AppStrings.Error_IncorrectCredentials);
 			return Page();
 		}
-		
+
 		var res = await _signInManager.PasswordSignInAsync(
 			user,
 			Form.Contraseña,
 			Form.Recordar,
 			false
 		);
-		
+
 		if (res.Succeeded) return Redirect(ReturnUrl ?? "/Index");
-		
-		if (res.RequiresTwoFactor) return RedirectToPage("/LoginChallenge", new { ReturnUrl,
-			RememberMe = Form.Recordar
-		});
-		
-		MensajeError = AppStrings.Error_IncorrectCredentials;
+		if (res.RequiresTwoFactor)
+			return RedirectToPage("/LoginChallenge", new
+			{
+				ReturnUrl,
+				RememberMe = Form.Recordar
+			});
+
+		ModelState.AddModelError(nameof(Form.Contraseña), AppStrings.Error_IncorrectCredentials);
 		return Page();
 	}
 }
 
 public class LoginForm
 {
+	[Required(ErrorMessageResourceType = typeof(AppStrings),
+		ErrorMessageResourceName = nameof(AppStrings.Validation_Email_Required))]
+	[EmailAddress(ErrorMessageResourceType = typeof(AppStrings),
+		ErrorMessageResourceName = nameof(AppStrings.Validation_Email_Required))]
 	public string Email { get; set; } = string.Empty;
+
+	[Required(ErrorMessageResourceType = typeof(AppStrings),
+		ErrorMessageResourceName = nameof(AppStrings.Validation_Password_Required))]
 	public string Contraseña { get; set; } = string.Empty;
+	
 	public bool Recordar { get; set; } = false;
 }
